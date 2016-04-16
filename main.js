eacd23e28a50ceffa5f0251fe2fdb80ad89ece1d
g-rocket <g.rocket1@gmail.com>
2016-04-15 21:37:44 -0700

    First attempt at website

---
+++ main.js
@@ -0,0 +1,32 @@
+/* Variables conncected with HTML */
+var rawText;
+var inputLanguage;
+var form = document.getElementById("inputForm");
+
+
+
+$("#submit").click(function() {
+	// Retrieve input and language
+	rawText = document.getElementById("input").value;
+	inputLanguage = document.getElementById("language").value;
+
+	// Parse into AST
+	switch(inputLanguage) {
+		case ast:
+			break;
+		case latex:
+			break;
+		case mathematica:
+			break;
+		case matlab:
+			break;
+	};
+
+	// Output
+
+});
+
+
+
+
+$(document).ready();
