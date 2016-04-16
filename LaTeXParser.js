ff791a3dd12435b2bc094a602a61ec49df6d5857
g_rocket <g.rocket1@gmail.com>
2016-04-16 00:13:00 -0700

    do more LaTeX parsing (not done)

--- LaTeXParser.js
+++ LaTeXParser.js
@@ -1,7 +1,5 @@
 parseLaTeXExpr = function(equation) {
-	tokenList = tokenizeLaTeX(parseExpr)
-	if(!tokenList) return false
-	return parseLaTeXTokenList(tokenList)
+	return parseLaTeXTokenList(tokenizeLaTeX(equation,0))
 }
 
 getCatcode = function(character) {
@@ -26,53 +24,71 @@ getCatcode = function(character) {
 	}
 }
 
-tokenizeLaTeX = function(equation) {
+tokenizeLaTeX = function(equation, start) {
 	var tokenList = []
-	for(var i = 0; i < equation.length; i++) {
-		switch(this.getCatcode(equation[i])) {
+	for(var i = start; i < equation.length; i++) {
+		switch(getCatcode(equation[i])) {
 			case 0:
 				var cmdName = ""
 				i++
-				for(;i < equation.length && this.getCatcode(equation[i]) == 11; i++) {
+				for(;i < equation.length && getCatcode(equation[i]) == 11; i++) {
 					cmdName += equation[i]
-					i++
 				}
-				tokenList.push({type: "command", name: cmdName})
+				i--
+				tokenList.push({mode: "token", type: "command", name: cmdName})
 				break
 			case 1:
-				tokenList.push({type: "beginGroup"})
+				subgroup = tokenizeLaTeX(equation, i+1)
+				tokenList.push({mode: "ASP", value: parseLaTeXTokenList(subgroup.tl)})
+				i = subgroup.end
 				break
 			case 2:
-				tokenList.push({type: "endGroup"})
+				if(start == 0) throw "Too many closing curly brackets"
+				return {tl:tokenList, end:i}
 				break
 			case 5:
 			case 10:
 				break
 			case 7:
-				tokenList.push({type: "superscript"})
+				tokenList.push({mode: "token", type: "superscript"})
 				break
 			case 8:
-				tokenList.push({type: "subscript"})
+				tokenList.push({mode: "token", type: "subscript"})
 				break
 			case 9: break
-			case 10:
-				tokenList.push({type: "letter", value: equation[i]})
 			case 11:
-				
+				tokenList.push({mode: "token", type: "letter", value: equation[i]})
+				break
+			case 12:
+				tokenList.push({mode: "token", type: "other", value: equation[i]})
+				break
 			case 13:
-				tokenList.push({type: "command", name: equation[i]})
+				tokenList.push({mode: "token", type: "command", name: equation[i]})
 				break
 			case 14:
 				i++
-				for(;i < equation.length && this.getCatcode(equation[i]) != 5; i++);
+				for(;i < equation.length && getCatcode(equation[i]) != 5; i++);
+				i--
 				break
 			default:
-				return false
+				console.log(equation[i], getCatcode(equation[i]), tokenList)
+				throw ("Illegal token: " + equation[i] + " (catcode "+getCatcode(equation[i])+")")
 		}
 	}
+	if(start > 0) throw "Too many opening curly brackets"
 	return tokenList
 }
 
 parseLaTeXTokenList = function(tokenList) {
-	
+	for(var i = 0; i < tokenList.length; i++) {
+		if(tokenList[i].mode !== "token") continue;
+		if(tokenList[i].type === "command") {
+			switch(tokenList[i].name) {
+				case "frac": case "dfrac": case "tfrac": case "rfrac": case "sfrac":
+				case "sqrt":
+				case "sin": case "cos": case "tan": case "acos": case "asin": case "atan":
+				case "":
+			}
+		}
+	}
 }
