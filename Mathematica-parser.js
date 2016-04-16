8bc302439e3e374ef742cc2566fa5151bbebf184
T Fong <tiffanyfong42@gmail.com>
2016-04-15 23:50:44 -0700

    Temporarily abandon parsing Mathematica

--- Mathematica-parser.js
+++ Mathematica-parser.js
@@ -1,3 +1,6 @@
+/* This is not going to work */
+
+
 parseMathematicaExpr = function(equation) {
 	tokenList = tokenizeMathematica(parseExpr)
 	if(!tokenList) return false
@@ -6,15 +9,22 @@ parseMathematicaExpr = function(equation) {
 
 getCatcode = function(character) {
 	switch(character) {
-		case "\\": return 0;
-		case "{": return 1;
+		//case "\\": return 0;
+
+		// x = latin character
+		// apple (string) = latin
+
+		case "{": return 1; // list args
+		case "[": return 1; // function TODO
 		case "}": return 2;
+		case "[": return 2;
 		case "$": return 3;
 		case "&": return 4;
-		case "\n": return 5;
-		case "#": return 6;
-		case "^": return 7;
-		case "_": return 8;
+		//case "\n": return 5; // does nothing
+		//case "#": return 6;
+		//case "$": return 6;  // MOTHERFUCKER
+		//case "^": return 7;
+		//case "_": return 8;
 		case "\0": return 9;
 		case " ":
 		case "\t": return 10;
@@ -30,29 +40,30 @@ tokenizeMathematica = function(equation) {
 	var tokenList = []
 	for(var i = 0; i < equation.length; i++) {
 		switch(this.getCatcode(equation[i])) {
+			/* Latex specific
 			case 0:
 				var cmdName = ""
 				i++
 				for(;i < equation.length && this.getCatcode(equation[i]) == 11; i++) {
 					cmdName += equation[i]
-					i++
 				}
 				tokenList.push({type: "command", name: cmdName})
 				break
+				*/
 			case 1:
 				tokenList.push({type: "beginGroup"})
 				break
 			case 2:
 				tokenList.push({type: "endGroup"})
 				break
-			case 5:
+			//case 5:
 			case 10:
 				break
 			case 7:
-				tokenList.push({type: "superscript"})
+				tokenList.push({type: "superscript"})		// OH HELL NO
 				break
 			case 8:
-				tokenList.push({type: "subscript"})
+				tokenList.push({type: "subscript"})			// WTF MATHEMATICA
 				break
 			case 9: break
 			case 13:
